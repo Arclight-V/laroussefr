0fa3f577b3b1f60d84c5dc6f42a8c777e5fa6490
VVKalanchin <vladimtor@gmail.com>
2024-12-22 18:38:43 +0300

    change import

--- traduction/match/match.go
+++ traduction/match/match.go
@@ -2,22 +2,36 @@
 // github.com/yhat/scrape.
 package match
 
-import "golang.org/x/html"
+import (
+	"github.com/yhat/scrape"
+	"golang.org/x/net/html"
+)
 
 // PhraseProp returns true if n is a node containing a Phrase property.
 func PhraseProp(n *html.Node) bool {
 	switch scrape.Attr(n, "class") {
-		case "Locution2":          fallthrough
-		case "Glose2":             fallthrough
-		case "Traduction2":        fallthrough
-		case "Renvois":            fallthrough
-		case "Metalangue2":        fallthrough
-		case "lienson3":           fallthrough
-		case "lienson2":           fallthrough
-		case "Indicateur":         fallthrough
-		case "IndicateurDomaine":  fallthrough
-		case "Metalangue":         fallthrough
-		case "DivisionExpression": return true
+	case "Locution2":
+		fallthrough
+	case "Glose2":
+		fallthrough
+	case "Traduction2":
+		fallthrough
+	case "Renvois":
+		fallthrough
+	case "Metalangue2":
+		fallthrough
+	case "lienson3":
+		fallthrough
+	case "lienson2":
+		fallthrough
+	case "Indicateur":
+		fallthrough
+	case "IndicateurDomaine":
+		fallthrough
+	case "Metalangue":
+		fallthrough
+	case "DivisionExpression":
+		return true
 	}
 	return false
 }
@@ -25,11 +39,16 @@ func PhraseProp(n *html.Node) bool {
 // MeaningProp returns true if n is a node containing a Meaning property.
 func MeaningProp(n *html.Node) bool {
 	switch scrape.Attr(n, "class") {
-		case "Traduction":         fallthrough
-		case "Glose2":             fallthrough // for en->fr "blue" POLITICS
-		case "Indicateur":         fallthrough
-		case "IndicateurDomaine":  fallthrough
-		case "Metalangue":         return true
+	case "Traduction":
+		fallthrough
+	case "Glose2":
+		fallthrough // for en->fr "blue" POLITICS
+	case "Indicateur":
+		fallthrough
+	case "IndicateurDomaine":
+		fallthrough
+	case "Metalangue":
+		return true
 	}
 	return false
 }
